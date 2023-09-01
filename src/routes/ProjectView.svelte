f1e812ddebb64f7d2b59125106e9481b6a770791
Demi Willison <65977881+hbgg@users.noreply.github.com>
2023-09-01 16:03:26 -0700

    Fix Projects Empty Space

--- src/routes/ProjectView.svelte
+++ src/routes/ProjectView.svelte
@@ -26,15 +26,24 @@
     </h1>
     <div class="text-center italic"><span class="font-semibold">NOTE:</span> This is by no means my complete backlog! Check back in 1-2 weeks to see the complete list (and demos 👀)</div>
 </div>
-
-<div class="w-full grid grid-rows-6 grid-cols-3 md:grid-cols-6 md:grid-rows-3 max-sm:h-[70rem] sm:h-[24rem] md:h-[30rem] lg:h-[52rem] align-top">
-{#each relevantProjects as project (project.url)}
-    <button animate:flip={{duration:400}} transition:fade on:click={() => {chosenProject.set(project)}} class="rounded-xl w-full text-center text-xl max-sm:px-2 sm:px-5  sm:py-5">
-        <img class="rounded-3xl sm:mb-2 max-w-full w-full align-middle border-none aspect-square object-cover" src={"projects\\"+ project.url + "\\icon.png"} alt={project.name} loading="lazy"/>
-        {project.name}
-    </button>
-{/each}
+<div class="w-full grid">
+    <div class="w-full flex flex-wrap align-top" style="grid-row:1/1; grid-column:1/1">
+    {#each relevantProjects as project (project.url)}
+        <button animate:flip={{duration:400}} transition:fade on:click={() => {chosenProject.set(project)}} class="rounded-xl max-md:w-1/3 md:w-1/6 text-center text-xl max-sm:px-2 sm:px-5  sm:py-5">
+            <img class="rounded-3xl sm:mb-2 max-w-full w-full align-middle border-none aspect-square object-cover" src={"projects\\"+ project.url + "\\icon.png"} alt={project.name} loading="lazy"/>
+            {project.name}
+        </button>
+    {/each}
+    </div>
+    <div class="w-full flex flex-wrap align-top invisible" style="grid-row:1/1; grid-column:1/1">
+        {#each Object.values(projects) as project (project.url)}
+            <button animate:flip={{duration:400}} transition:fade on:click={() => {chosenProject.set(project)}} class="rounded-xl max-md:w-1/3 md:w-1/6 text-center text-xl max-sm:px-2 sm:px-5 my-1 sm:py-5">
+                <img class="rounded-3xl sm:mb-2 max-w-full w-full align-middle border-none aspect-square object-cover" src={"projects\\"+ project.url + "\\icon.png"} alt={project.name} loading="lazy"/>
+                {project.name}
+            </button>
+        {/each}
+    </div>
+</div>
 {#each Object.values(projects) as project}
 <ProjectModal project={project} />
 {/each}
-</div>
