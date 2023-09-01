c02f3c362d5a4c320d4392910e1d32b978879a1a
Demi Willison <65977881+hbgg@users.noreply.github.com>
2023-09-01 16:11:24 -0700

    Fix Vertical Alignment of Projects View

--- src/routes/ProjectView.svelte
+++ src/routes/ProjectView.svelte
@@ -27,7 +27,7 @@
     <div class="text-center italic"><span class="font-semibold">NOTE:</span> This is by no means my complete backlog! Check back in 1-2 weeks to see the complete list (and demos 👀)</div>
 </div>
 <div class="w-full grid">
-    <div class="w-full flex flex-wrap align-top" style="grid-row:1/1; grid-column:1/1">
+    <div class="w-full flex flex-wrap align-top items-start" style="grid-row:1/1; grid-column:1/1">
     {#each relevantProjects as project (project.url)}
         <button animate:flip={{duration:400}} transition:fade on:click={() => {chosenProject.set(project)}} class="rounded-xl max-md:w-1/3 md:w-1/6 text-center text-xl max-sm:px-2 sm:px-5  sm:py-5">
             <img class="rounded-3xl sm:mb-2 max-w-full w-full align-middle border-none aspect-square object-cover" src={"projects\\"+ project.url + "\\icon.png"} alt={project.name} loading="lazy"/>
